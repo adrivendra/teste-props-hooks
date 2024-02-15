6795b561fe98bfe4c2089b07c631e93f9134494a
Adriano <adrianovendramini@hotmail.com>
2024-02-15 11:06:55 -0300

    renderizacao condicional

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,23 +1,16 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 function Home() {
-  const [completa, setCompleta] = useState(false)
-  const [tarefa, setTarefa] = useState('')
-
-  useEffect(() => {
-    if (completa) {
-      setTarefa('Parabéns! Você concluiu a tarefa!')
-    }
-  }, [completa])
+  const [Login, setLogin] = useState(false)
 
   return (
     <div>
-      <h1>Tarefa</h1>
-      <h3>{tarefa}</h3>
-      <p>Conclua a tarefa</p>
-      <button onClick={() => setCompleta(true)}>Concluir Tarefa</button>
+      {Login ? (
+        <h1>Bem-vindo de volta!</h1>
+      ) : (
+        <button onClick={() => setLogin(true)}>Entrar</button>
+      )}
     </div>
   )
 }
-
 export default Home
